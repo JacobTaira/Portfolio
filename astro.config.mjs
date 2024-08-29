e0eb5798d501d8566f80e9ad0d9a23178c8d2ef8
MushiTea <94145946+MushiTea@users.noreply.github.com>
2024-08-29 14:53:10 -0500

    Github workflow to update site on Github Pages every push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://JacobTaira.github.io',
+    base: 'Portfolio',
+    /*
+    If the base is set to 'my-repo', and you have an image with a relative URL /images/photo.jpg, 
+    Astro will automatically prepend the base, resulting in the URL /my-repo/images/photo.jpg.
+    */
+});
