ccd1bcdcdc249cc83c1e5ae87320e92e68420b47
MushiTea <94145946+MushiTea@users.noreply.github.com>
2024-08-29 20:08:36 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://JacobTaira.github.io',
-    base: 'Portfolio',
+    base: 'Portfolio', // you want to actually publish this specific repo, not the account (jacobtaira.github.io)
     /*
     If the base is set to 'my-repo', and you have an image with a relative URL /images/photo.jpg, 
     Astro will automatically prepend the base, resulting in the URL /my-repo/images/photo.jpg.
