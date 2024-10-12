a2b81e74356cb65dca5f47266a9877353c4bbe44
JacobTaira <jacob.m.taira@gmail.com>
2024-10-12 15:08:31 -0500

    Refined CSS, added ideas, home, and about me pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 
 export default defineConfig({
-    site: 'https://JacobTaira.github.io',
-    base: 'Portfolio', // you want to actually publish this specific repo, not the account (jacobtaira.github.io)
-    /*
-    
-    
-    If the base is set to 'my-repo', and you have an image with a relative URL /images/photo.jpg, 
-    Astro will automatically prepend the base, resulting in the URL /my-repo/images/photo.jpg.
-    */
+  site: 'https://JacobTaira.github.io',
+
+  // you want to actually publish this specific repo, not the account (jacobtaira.github.io)
+  base: 'Portfolio'
+  /*
+  
+  
+  If the base is set to 'my-repo', and you have an image with a relative URL /images/photo.jpg, 
+  Astro will automatically prepend the base, resulting in the URL /my-repo/images/photo.jpg.
+  */,
+
+  integrations: [tailwind()]
 });
